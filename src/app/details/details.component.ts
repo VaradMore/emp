5574ab89b4fedc1339e30b6a838ee4e4e7fac326
VaradMore <varad2720@gmail.com>
2018-08-06 20:07:20 +0530

    s commit

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -7,8 +7,9 @@ import { ActivatedRoute  } from '@angular/router';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
+
 export class DetailsComponent implements OnInit {
-  user$: Object;
+  user$: any;
 
   constructor(private data: DataService, private route: ActivatedRoute) { 
     this.route.params.subscribe( params => this.user$ = params.id)
